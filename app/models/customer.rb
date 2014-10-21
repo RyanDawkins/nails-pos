d4441020f295c6f352ebd9b0aaf78fd6ba69d29d
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-21 14:30:53 -0500

    Added the increment feature and wrote more tests to ensure it was working correctly. Also added the visit_count to be a attr_accessor so that it would be publicly accessible to modify outside of the object and in methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,15 @@
 class Customer < ActiveRecord::Base
 
+  attr_accessor :visit_count
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
+  def increment_visit
+    if @visit_count.nil?
+      @visit_count = 0
+    end
+    @visit_count = @visit_count + 1
+  end
+
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -23,15 +23,32 @@ class CustomerTest < ActiveSupport::TestCase
 
   end
 
-  test "increments by one" do
-
+  test "increments to one from nil" do
     customer = get_customer_without :visit_count
-    customer.visit_count = 0
 
     customer.increment_visit
 
-    assert_equal customer.visit_count, 1
+    assert_equal 1, customer.visit_count
+  end
 
+  test "increments by one from number" do
+
+    # Customer has a visit_count of 1
+    customer = get_customer_without :nil
+
+    # Should make visit count to 2
+    customer.increment_visit
+
+    assert_equal 2, customer.visit_count
+  end
+
+  test "increments from 10 to 11" do
+
+    customer = get_customer_without :visit_count
+    customer.visit_count = 10
+    customer.increment_visit
+
+    assert_equal 11, customer.visit_count
   end
 
   private
