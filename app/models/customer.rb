499adc3c8de23c69e84f814eced0a7434a5c50a4
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-22 17:47:56 -0500

    Fixed bug with attr_accessor

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,16 +1,14 @@
 class Customer < ActiveRecord::Base
 
-  attr_accessor :visit_count
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   def increment_visit
-    if @visit_count.nil?
-      @visit_count = 0
+    if self.visit_count.nil?
+      self.visit_count = 0
     end
-    @visit_count = @visit_count + 1
+    self.visit_count = self.visit_count + 1
   end
 
 end
