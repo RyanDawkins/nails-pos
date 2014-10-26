b27b27c42ed3952bf294bf1832b6fc6f3a669d41
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-26 13:18:53 -0500

    Made a more reusable rest controller

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,55 +1,8 @@
 class CustomerController < RestController
 
-  before_filter :find, :only => [:show, :update, :delete]
-
-  def index
-    @customers = Customer.all
-    render json: @customers
-  end
-
-  def show
-    if @customer
-      render json: @customer
-    else
-      render json: error_message("Couldn't find your customer")
-    end
-  end
-
-  def create
-
-    @customer = Customer.new from_params
-    if @customer.save
-      render json: @customer
-    else
-      render json: error_message("Didn't send the correct params")
-    end
-  end
-
-  def update
-    if @customer and (@customer.update from_params) and @customer.save
-      render json: @customer
-    else
-      render json: error_message("We had a problem saving your customer")
-    end
-  end
-
-  def delete
-    if @customer
-      render json: @customer.destroy
-    else
-      render json: error_message("We had a problem finding your customer")
-    end
-  end
-
   private
-  def find
-    if params[:id]
-      begin
-        @customer = Customer.find(params[:id])
-      rescue
-        @customer = nil
-      end
-    end
+  def get_class
+    Customer
   end
 
   def from_params
--- app/controllers/rest_controller.rb
+++ app/controllers/rest_controller.rb
@@ -1,5 +1,56 @@
 class RestController < ApplicationController
 
+  before_filter :find, :only => [:show, :update, :delete]
+
+  def index
+    @objects = get_class().all
+    render json: @objects
+  end
+
+  def show
+    if @object
+      render json: @object
+    else
+      render json: error_message("Couldn't find your #{get_class}")
+    end
+  end
+
+  def create
+
+    @object = get_class.new from_params
+    if @object.save
+      render json: @object
+    else
+      render json: error_message("Didn't send the correct params")
+    end
+  end
+
+  def update
+    if @object and (@object.update from_params) and @object.save
+      render json: @object
+    else
+      render json: error_message("We had a problem saving your #{get_class}")
+    end
+  end
+
+  def delete
+    if @object
+      render json: @object.destroy
+    else
+      render json: error_message("We had a problem finding your customer")
+    end
+  end
+
+  def find
+    if params[:id]
+      begin
+        @object = get_class.find(params[:id])
+      rescue
+        @object = nil
+      end
+    end
+  end
+
   def error_message message
     {
       message: message
