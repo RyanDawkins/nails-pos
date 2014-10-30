9f7600b4e22a36067ab7140b97cb00c4b66a2ea1
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-30 02:16:46 -0500

    Created tests and passing validations for services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,11 @@
 class Service < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :cost,
+    presence: true,
+    :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ },
+    :numericality => {
+      :greater_than_or_equal_to => 0
+    }
+
 end
--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "can't save blank" do
+    service = Service.new
+    assert_not service.save
+  end
+
+  test "can't save without name" do
+    service = get_without :name
+    assert_not service.save
+  end
+
+  test "can't save without a cost" do
+    service = get_without :cost
+    assert_not service.save
+  end
+
+  test "can't save decimal with more than 2 precision" do
+    service = get_without :cost
+    service.cost = 10.999
+    assert_not service.save
+  end
+
+  test "can't save negative values" do
+    service = get_without :cost
+    service.cost = -1
+    assert_not service.save
+  end
+
+  private
+  def get_without attr
+
+    service = Service.new
+
+    if attr == :name
+      service.name = "Pedicure"
+    end
+
+    if attr == :cost
+      service.cost = 10.99
+    end
+
+    service
+  end
+
 end
