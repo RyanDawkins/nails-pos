28049265b45d5b24e3efbd065385d3455ce8eee4
Kyle Riedemann <kylealanr@gmail.com>
2014-11-07 01:51:28 -0600

    wrote passing tests

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,17 @@
 class Schedule < ActiveRecord::Base
+
+  validates :appointment_id, presence: true, :numericality => {
+    :only_integer => true,
+    :greater_than => 0
+  }
+
+  validates :service_id, presence: true, :numericality => {
+    :only_integer => true,
+    :greater_than => 0
+  }
+
+  validates :cost, presence: true
+
+  validates :name, presence: true
+
 end
