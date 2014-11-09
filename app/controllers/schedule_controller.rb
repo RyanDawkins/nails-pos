4a46437edf1220ccf818493dd95a20228d128cd1
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-09 14:56:28 -0600

    Updated schedule controller to accept service_id's instead of actual values

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,11 +1,63 @@
-class ScheduleController < ApplicationController
+class ScheduleController < RestController
 
+  before_filter :get_service, :only => [:create, :update]
+
+  def create
+    if params[:appointment_id] and copy_and_save(params[:appointment_id])
+      render json: @object
+    else
+      render json: error_message("Didn't send correct parameters")
+    end
+  end
+
+  def update
+    if copy_and_save
+      render json: @object
+    else
+      render json: error_message("Didn't send correct parameters")
+    end
+  end
+
+  private
   def get_class
     Schedule
   end
 
   def from_params
-    params.permit(:appointment_id, :service_id, :name, :cost)
+    params.permit(:appointment_id, :service_id)
+  end
+
+  def get_service
+    if params[:service_id]
+      begin
+        @service = Service.find(params[:service_id])
+      rescue
+        nil
+      end
+    end
+  end
+
+  # Copying the service to the schedule as a historical
+  # record.
+  def copy_and_save appointment_id=nil
+
+    unless @service
+      return false
+    end
+
+    if @object.nil?
+      if appointment_id.nil?
+        return false
+      end
+      @object = get_class.new
+      @object.appointment_id = appointment_id
+    end
+
+    @object.service_id = @service.id
+    @object.name = @service.name
+    @object.cost = @service.cost
+
+    @object.save
   end
 
 end
--- test/controllers/schedule_controller_test.rb
+++ test/controllers/schedule_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ScheduleControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create" do
+    post :create
+    assert_response :success
+  end
+
+  test "update" do
+    post :update, :format => :json, :id => 1
+    assert_response :success
+  end
+
 end
