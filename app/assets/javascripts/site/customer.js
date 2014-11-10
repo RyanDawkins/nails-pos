6198d70aea847af7ec68887dd1d77e96404acb96
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-10 14:20:55 -0600

    Added jshint to customer

--- app/assets/javascripts/site/customer.js
+++ app/assets/javascripts/site/customer.js
@@ -1,3 +1,4 @@
+/* global app */
 app.controller("CustomerTableController", function($scope, $http){
 
   $scope.customers = [];
@@ -7,9 +8,11 @@ app.controller("CustomerTableController", function($scope, $http){
 
   var loadTable = function() {
     $http.get('/customer').
-      success(function(data, status, headers, config) {
+    /*jshint unused:false */
+    success(function(data, status, headers, config) {
         $scope.customers = data;
     }).
+    /*jshint unused:false */
     error(function(data, status, headers, config) {
       // called asynchronously if an error occurs
       // or server returns response with an error status.
@@ -38,10 +41,12 @@ app.controller("CustomerTableController", function($scope, $http){
     var customer = $scope.newCustomer;
     $scope.newCustomer = {};
     $http.post('/customer/create', customer).
-      success(function(data, status, headers, config) {
+    /*jshint unused:false */
+    success(function(data, status, headers, config) {
         loadTable();
         toggleCreating();
     }).
+    /*jshint unused:false */
     error(function(data, status, headers, config) {});
   };
 
@@ -49,18 +54,22 @@ app.controller("CustomerTableController", function($scope, $http){
     var customer = $scope.newCustomer;
     $scope.newCustomer = {};
     $http.post('/customer/'+customer.id+"/update", customer).
-      success(function(data, status, headers, config) {
+    /*jshint unused:false */
+    success(function(data, status, headers, config) {
         loadTable();
         toggleCreating();
     }).
+    /*jshint unused:false */
     error(function(data, status, headers, config) {});
   };
 
   $scope.delete = function(customer) {
     $http.get('/customer/'+customer.id+'/delete').
-      success(function(data, status, headers, config) {
+    /*jshint unused:false */
+    success(function(data, status, headers, config) {
         loadTable();
     }).
+    /*jshint unused:false */
     error(function(data, status, headers, config) {});
   };
 
