5174898f2aa804a9b45174e688ab8c38c5d66843
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-11 13:31:19 -0600

    Added jshint annotations

--- app/assets/javascripts/site/schedule.js
+++ app/assets/javascripts/site/schedule.js
@@ -1,15 +1,17 @@
-var resize = function(element) {
+var resize = function() {
+  /* unused=false */
   $(".table-square > tbody > tr > td").each(function(event){
     $(this).height($(this).width() / 2);
   });
 }
 
 
-
+/* unused=false */
 $(document).ready(function(event){
   resize();
 });
 
+/* unused=false */
 $(window).resize(function(event) {
   resize();
 });
