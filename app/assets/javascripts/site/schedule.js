e7f618f93378b393e4f62b1737c7607dca3dede5
Kyle Riedemann <kylealanr@gmail.com>
2014-11-12 00:53:13 -0600

    created working sample sortable table based on Angular documentation

--- app/assets/javascripts/site/schedule.js
+++ app/assets/javascripts/site/schedule.js
@@ -1,17 +1,14 @@
-var resize = function() {
-  /*jshint unused:false */
-  $(".table-square > tbody > tr > td").each(function(event){
-    $(this).height($(this).width() / 2);
-  });
-};
-
-
-/*jshint unused:false */
-$(document).ready(function(event){
-  resize();
-});
-
-/*jshint unused:false */
-$(window).resize(function(event) {
-  resize();
-});
+app.controller('ScheduleController', ['$scope', '$filter', function($scope, $filter) {
+  var orderBy = $filter('orderBy');
+  $scope.friends = [
+    { name: 'Kyle', phone: '471-3895' },
+    { name: 'Ryan', phone: '555-5555' },
+    { name: 'Aaron', phone: '555-5555' },
+    { name: 'Hali', phone: '555-5555' },
+    { name: 'Marcus', phone: '555-5555' },
+    { name: 'Carissa', phone: '555-5555' },
+  ];
+  $scope.order = function(predicate, reverse) {
+    $scope.friends = orderBy($scope.friends, predicate, reverse);
+  };
+}]);
