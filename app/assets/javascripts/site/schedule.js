5d3332efb74b064981a13467467572b3cb3b04d2
Kyle Riedemann <kylealanr@gmail.com>
2014-11-14 00:03:47 -0600

    rewrote sort function for table rows and added indicator for reverse sort

--- app/assets/javascripts/site/schedule.js
+++ app/assets/javascripts/site/schedule.js
@@ -1,14 +1,15 @@
-app.controller('ScheduleController', ['$scope', '$filter', function($scope, $filter) {
-  var orderBy = $filter('orderBy');
-  $scope.friends = [
-    { name: 'Kyle', phone: '471-3895', date: '1:00', service: 'service' },
-    { name: 'Ryan', phone: '111-1111', date: '2:00', service: 'service' },
-    { name: 'Aaron', phone: '222-2222', date: '3:00', service: 'service' },
-    { name: 'Hali', phone: '333-3333', date: '4:00', service: 'service' },
-    { name: 'Marcus', phone: '444-44444', date: '5:00', service: 'service' },
-    { name: 'Carissa', phone: '555-5555', date: '6:00', service: 'service' },
-  ];
-  $scope.order = function(predicate, reverse) {
-    $scope.friends = orderBy($scope.friends, predicate, reverse);
+app.controller('ScheduleController', function($scope) {
+  $scope.orderByField = 'lastName';
+  $scope.reverseSort = false;
+
+  $scope.data = {
+    schedules: [
+      { firstName: 'Kyle', lastName: 'Riedemann', phone: '471-3895', date: '1:00', service: 'service' },
+      { firstName: 'Ryan', lastName: 'Dawkins', phone: '111-1111', date: '2:00', service: 'service' },
+      { firstName: 'Aaron', lastName: 'Weaver', phone: '222-2222', date: '3:00', service: 'service' },
+      { firstName: 'Hali', lastName: 'Deubler', phone: '333-3333', date: '4:00', service: 'service' },
+      { firstName: 'Marcus', lastName: 'Gabilheri', phone: '444-44444', date: '5:00', service: 'service' },
+      { firstName: 'Carissa', lastName: 'Gabilheri', phone: '555-5555', date: '6:00', service: 'service' }
+    ]
   };
-}]);
+});
