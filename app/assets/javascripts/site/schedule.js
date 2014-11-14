08561dc2394096f8a32307ebf4eb2f300345d73f
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-14 12:33:58 -0600

    Added ability to return relations

--- app/assets/javascripts/site/schedule.js
+++ app/assets/javascripts/site/schedule.js
@@ -1,15 +1,20 @@
-app.controller('ScheduleController', function($scope) {
-  $scope.orderByField = 'lastName';
+/* global app */
+app.controller('ScheduleController', function($scope, $http) {
+  $scope.orderByField = 'date';
   $scope.reverseSort = false;
 
-  $scope.data = {
-    schedules: [
-      { firstName: 'Kyle', lastName: 'Riedemann', phone: '471-3895', date: '1:00', service: 'service' },
-      { firstName: 'Ryan', lastName: 'Dawkins', phone: '111-1111', date: '2:00', service: 'service' },
-      { firstName: 'Aaron', lastName: 'Weaver', phone: '222-2222', date: '3:00', service: 'service' },
-      { firstName: 'Hali', lastName: 'Deubler', phone: '333-3333', date: '4:00', service: 'service' },
-      { firstName: 'Marcus', lastName: 'Gabilheri', phone: '444-44444', date: '5:00', service: 'service' },
-      { firstName: 'Carissa', lastName: 'Gabilheri', phone: '555-5555', date: '6:00', service: 'service' }
-    ]
+  $scope.toggleSort = function(orderByField) {
+    $scope.orderByField = orderByField;
+    $scope.reverseSort = !$scope.reverseSort;
+  };
+
+  $scope.data = {};
+  $scope.fillTable = function() {
+    $http.get('/appointment').success(function(data){
+      $scope.data = data;
+      console.log(data[0]);
+    }).error(function(data){
+      console.log(data);
+    });
   };
 });
