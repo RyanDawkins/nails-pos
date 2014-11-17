ded6388c3b3b38d00c76255532d10aa060d52385
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-16 23:45:12 -0600

    Fixed helper

--- app/helpers/twillio_helper.rb
+++ app/helpers/twillio_helper.rb
@@ -1,17 +1,19 @@
 require 'twilio-ruby'
 
-class TwillioBase
-	def self.createClient
-		@@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+module TwilioHelper
+	class TwillioBase
+		def self.createClient
+			@@client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+		end
 	end
-end
 
-class TwillioSMS < TwillioBase
-	def self.sendSMS(recipient, content)
-		TwillioBase.createClient
-		@message = @@client.account.messages.create({
-			to: recipient,
-			from: ENV["TWILIO_SENT_NUMBER"],
-			body: content })
+	class TwillioSMS < TwillioBase
+		def self.sendSMS(recipient, content)
+			TwillioBase.createClient
+			@message = @@client.account.messages.create({
+				to: recipient,
+				from: ENV["TWILIO_SENT_NUMBER"],
+				body: content })
+		end
 	end
 end
