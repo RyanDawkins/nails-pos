3cacee37c9cb7326cee0fde44883fc699369ed88
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-16 21:46:12 -0600

    Added ability for appointment to look for a date

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -13,4 +13,13 @@ class AppointmentController < RestController
     [:customer, :employee]
   end
 
+  def all
+    if params[:date]
+      day = Time.parse params[:date]
+      Time.zone=-6
+      @objects = get_class().where(date: day.beginning_of_day..day.end_of_day)
+    else
+      @objects = get_class().all
+    end
+  end
 end
